3c06a3714fe94a91fe2cf9d5f5d37c9603c2c2ee
Rayan8992 <rayan.lima3@etec.sp.gov.br>
2025-11-28 15:53:35 -0300

    Update operacoesmatematicas_test.c

--- operacoesmatematicas_test.c
+++ operacoesmatematicas_test.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <stdio.h>
 
 int multiplicar(int a, int b);
 int dividir(int a, int b);
@@ -6,10 +7,18 @@ int dividir(int a, int b);
 int main() {
 
     assert(multiplicar(2, 3) == 6);
-    assert(multiplicar(-4, 5) == -20);
+    assert(multiplicar(5, 0) == 0);
+    assert(multiplicar(-4, 3) == -12);
+    assert(multiplicar(-4, -2) == 8);
 
     assert(dividir(10, 2) == 5);
-    assert(dividir(-9, 3) == -3);
+    assert(dividir(9, 3) == 3);
+    assert(dividir(8, 4) == 2);
+    assert(dividir(15, 3) == 5);
+    assert(dividir(-10, 2) == -5);
+    assert(dividir(-12, -4) == 3);
+
+    printf("Todos os testes passaram!\n");
 
     return 0;
 }
