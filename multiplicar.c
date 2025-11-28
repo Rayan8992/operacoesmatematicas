f018e8bb92b2d51528427d22adc496f90a2d3a9f
Rayan8992 <rayan.lima3@etec.sp.gov.br>
2025-11-28 15:52:11 -0300

    Update multiplicar.c

--- multiplicar.c
+++ multiplicar.c
@@ -1,3 +1,19 @@
 int multiplicar(int a, int b) {
-    return a * b;
+    int resultado = 0;
+
+    int negativo = 0;
+    if (b < 0) {
+        b = -b;
+        negativo = 1;
+    }
+
+    for (int i = 0; i < b; i++) {
+        resultado = resultado + a;
+    }
+
+    if (negativo) {
+        resultado = -resultado;
+    }
+
+    return resultado;
 }
