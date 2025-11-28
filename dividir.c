fbe0e912eaf690d2c4189fa1c3cc29d6739661d1
Rayan8992 <rayan.lima3@etec.sp.gov.br>
2025-11-28 15:52:50 -0300

    Update dividir.c

--- dividir.c
+++ dividir.c
@@ -1,9 +1,29 @@
-#include <stdio.h>
-
-int dividir(int a, int b) {
-    if (b == 0) {
-        printf("Erro: divisao por zero!\n");
+int dividir(int numero1, int numero2) {
+    if (numero2 == 0) {
         return 0;
     }
-    return a / b;
+
+    int quociente = 0;
+    int negativo = 0;
+
+    if (numero1 < 0) {
+        numero1 = -numero1;
+        negativo = !negativo;
+    }
+
+    if (numero2 < 0) {
+        numero2 = -numero2;
+        negativo = !negativo;
+    }
+
+    while (numero1 >= numero2) {
+        numero1 = numero1 - numero2;
+        quociente++;
+    }
+
+    if (negativo) {
+        quociente = -quociente;
+    }
+
+    return quociente;
 }
